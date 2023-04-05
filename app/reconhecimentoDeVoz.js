61fecbec996d40bf679f1fa6c92342d851f56703
Soraia Barbosa <so_raia_super@hotmail.com>
2023-04-05 18:53:07 -0300

    evento de reconhecimento de voz

--- app/reconhecimentoDeVoz.js
+++ app/reconhecimentoDeVoz.js
@@ -4,3 +4,9 @@ webkitSpeechRecognition;
 const recognition = new SpeechRecognition();
 recognition.lang = 'pt-Br'
 recognition.start()
+
+recognition.addEventListener('result', onSpeak)
+
+function onSpeak(e) {
+    console.log(e.results [0] [0].transcript)
+}
