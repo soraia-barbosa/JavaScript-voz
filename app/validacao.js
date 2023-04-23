05cfa2bfcb56f26a210447ccf8143b4bed04ea26
Soraia Barbosa <so_raia_super@hotmail.com>
2023-04-23 20:43:15 -0300

    lógica

--- app/validacao.js
+++ app/validacao.js
@@ -2,14 +2,30 @@ function verificaSeOChutePossuiUmValorValido(chute){
     const numero = +chute
 
     if (chuteInvalido(numero)){
-        console.log('Valor inválido')
+        elementoChute.innerHTML += '<div>Valor inválido</div'
     }
 
     if (numeroMaiorOuMenorQueValorPermitido(numero)){
-        (`Valor inválido: o número secreto precisa estar 
-        entre ${menorValor} e ${maiorValor}`)
+        elementoChute.innerHTML += `
+        <div>Valor inválido: fale um número entre ${menorValor} e ${maiorValor}</div>
+        `
     }
 }
+
+    if (numero === numeroSecreto){
+        document.body.innerHTML= `
+        <h2>Você acertou!</h2>
+        <h3>O número secreto era ${numeroSecreto}.</h3>
+        `
+    }else if (numero > numeroSecreto){
+        elementoChute.innerHTML +=`
+        <div>O número secreto é menor <i class="fa-solid fa-up-long"></i></div>
+        `
+    } else {
+        elementoChute.innerHTML +=`
+        <div>O número secreto é maior <i class="fa-solid fa-up-long"></i></div>)
+        `
+    }
 
 function chuteInvalido(numero){
     return Number.isNaN(numero)
